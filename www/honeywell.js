bba590d4d0883bc82be0c11d21be7328b249b5da
Jared Mathews <jared.mathews@datasplice.com>
2016-05-18 15:46:40 -0600

    Updated enable method for handling connection status
    Updated functionality within enable method for determining if the Captuvo is connected, and handling scan events.

--- www/honeywell.js
+++ www/honeywell.js
@@ -3,14 +3,34 @@ var exec = require("cordova/exec");
 function HoneywellScanner() {
 };
 
-HoneywellScanner.prototype.enable = function(callback) {
-  var sendResult = function(result) {
-    var data = JSON.parse(result);
-    callback(data);
-  }
-  // set up a global callback that is accessible from the native side
-  exec(sendResult, null, 'HoneywellScanner', 'registerCallback', [] );
-};
+HoneywellScanner.prototype.enable = function(successCallback, errorCallback, onScan) {
+    var noOnScan = !onScan;
+    if (noOnScan) { onScan = successCallback; }
+
+    var handleCallback = function(result) {
+      var data = JSON.parse(result);
+
+      if(data.status && noOnScan) {
+        if (data.status === 'CONNECTED') {
+          successCallback();
+        }
+      } else if (data.status) {
+          switch(data.status) {
+          case "CONNECTED":
+            successCallback(data.status);
+            break;
+
+          default:
+            errorCallback("Captuvo is Not Connected");
+          }
+      }
+      else if (data.data) {
+        onScan(data.data);
+      }
+    }
+
+    exec(handleCallback,null,'HoneywellScanner','registerCallback',[]);
+  };
 
 HoneywellScanner.prototype.disable = function() {
   delete cordova._captuvoCallback;
